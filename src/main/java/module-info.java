17382ddfcba471584422065eb5e247811cede571
Voinea Radu <contact@voinearadu.com>
2024-10-24 01:44:46 +0300

    Moved from com.voinearadu to com/voinearadu. Added some generic utils

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.voinearadu.utils {
     exports com.voinearadu.event_manager;
     exports com.voinearadu.file_manager;
     exports com.voinearadu.lambda;
+    exports com.voinearadu.lambda.lambda;
     exports com.voinearadu.logger;
+    exports com.voinearadu.logger.dto;
     exports com.voinearadu.message_builder;
     exports com.voinearadu.redis_manager;
     exports com.voinearadu.reflections;
+    exports com.voinearadu.generic;
 
     requires static lombok;
     requires static org.jetbrains.annotations;
