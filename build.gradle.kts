a1b996cc71758ec1e88ab935e1083046f1e6d8d1
Voinea Radu <contact@voinearadu.com>
2025-01-12 02:53:46 +0200

    Updated to add support for maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import cn.lalaki.pub.BaseCentralPortalPlusExtension
+
 plugins {
     id("java")
     id("java-library")
     id("maven-publish")
+    id("cn.lalaki.central") version "1.2.5"
+    id("signing")
 }
 
 group = libs.versions.group.get()
@@ -48,24 +52,56 @@ tasks {
     }
 }
 
+tasks.register("javadocJar", Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.named("javadoc"))
+}
+
+tasks.register("sourcesJar", Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
+val localRepo = uri("local-repo")
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+            artifact(tasks.named("sourcesJar"))
+            artifact(tasks.named("javadocJar"))
+
+            pom{
+                name.set("Utils Library")
+                description.set("A utility library for various purposes.")
+                url.set("https://github.com/Voinea-Radu/Utils")
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/Voinea-Radu/Utils/blob/master/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("voinearadu")
+                        name.set("Voinea Radu-Mihai")
+                        email.set("contact@voinearadu.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Voinea-Radu/Utils.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/Voinea-Radu/Utils.git")
+                    url.set("https://github.com/Voinea-Radu/Utils")
+                }
+            }
         }
     }
 
     repositories {
-        if (project.properties["com.voinearadu.publish"] == "true") {
-            maven(url = (project.findProperty("com.voinearadu.url") ?: "") as String) {
-                name = "VoineaRaduRepository"
-                credentials(PasswordCredentials::class) {
-                    username = (project.findProperty("com.voinearadu.auth.username") ?: "") as String
-                    password = (project.findProperty("com.voinearadu.auth.password") ?: "") as String
-                }
-            }
-        }
         if (project.properties["generic.publish"] == "true") {
             maven(url = (project.findProperty("generic.url") ?: "") as String) {
                 name = "GenericRepository"
@@ -75,6 +111,20 @@ publishing {
                 }
             }
         }
+
+        maven {
+            url = localRepo
+        }
     }
 }
 
+signing {
+    sign(publishing.publications["maven"])
+}
+
+centralPortalPlus {
+    url = localRepo
+    username = project.findProperty("maven.auth.username") as String
+    password = project.findProperty("maven.auth.password") as String
+    publishingType = BaseCentralPortalPlusExtension.PublishingType.AUTOMATIC
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
 rootProject.name = "utils"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
